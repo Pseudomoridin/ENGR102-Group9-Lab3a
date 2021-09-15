19ec7acf2217d872e555823df593dc00ef5a8386
Bryce Elphingstone <73117281+Pseudomoridin@users.noreply.github.com>
2021-09-15 20:16:38 +0000

    Fixed header

--- Lab3a_Act1_d.py
+++ Lab3a_Act1_d.py
@@ -2,9 +2,9 @@
 #   "Aggies do not lie, cheat, or steal, or tolerate those who do."
 #   "I have not given or received any unauthorized aid on this assignment."
 # Names:        Elayne Elphingstone
-#               NAME OF TEAM MEMBER 2
-#               NAME OF TEAM MEMBER 3
-#               NAME OF TEAM MEMBER 4
+#               Reagan Wall
+#               Logan Winship
+#               Tyler Mayou & Zack Abbott
 # Section:      472/572
 # Assignment:   Lab 2A Activity 3
 # Date:         September 8, 2021
